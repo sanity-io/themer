5733af056ef8a0e0b01ee90e7257acd6acf725a2
Cody Olsen <stipsan@gmail.com>
2022-08-02 11:22:24 +0200

    fix: add meta tags

--- examples/next-static/pages/[[...index]].tsx
+++ examples/next-static/pages/[[...index]].tsx
@@ -1,6 +1,32 @@
-import config from '../sanity.config'
+import Head from 'next/head'
 import { Studio } from 'sanity'
 
+import config from '../sanity.config'
+
 export default function IndexPage() {
-  return <Studio config={config} />
+  return (
+    <>
+      <Studio config={config} />
+      <Head>
+        {/* Studio implements display cutouts CSS (The iPhone Notch ™ ) and needs `viewport-fit=covered` for it to work correctly */}
+        <meta
+          name="viewport"
+          content="width=device-width, viewport-fit=cover"
+        />
+        {/* These theme-color tags makes the Studio look really really good on devices like iPads as the browser chrome adopts the Studio background */}
+        <meta
+          key="theme-color-light"
+          name="theme-color"
+          content={config.theme.color.light.default.base.bg}
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          key="theme-color-dark"
+          name="theme-color"
+          content={config.theme.color.dark.default.base.bg}
+          media="(prefers-color-scheme: dark)"
+        />
+      </Head>
+    </>
+  )
 }

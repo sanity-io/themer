dfaf54f4e83d8ba59f59da7401679e60a44e074e
Cody Olsen <stipsan@gmail.com>
2022-08-06 08:47:52 +0200

    test the new lib

--- examples/next-static/pages/[[...index]].tsx
+++ examples/next-static/pages/[[...index]].tsx
@@ -1,32 +1,7 @@
-import Head from 'next/head'
-import { Studio } from 'sanity'
+import { StudioPageLayout } from '@sanity/next-studio-layout'
 
 import config from '../sanity.config'
 
 export default function IndexPage() {
-  return (
-    <>
-      <Studio config={config} />
-      <Head>
-        {/* Studio implements display cutouts CSS (The iPhone Notch ™ ) and needs `viewport-fit=covered` for it to work correctly */}
-        <meta
-          name="viewport"
-          content="width=device-width, viewport-fit=cover"
-        />
-        {/* These theme-color tags makes the Studio look really really good on devices like iPads as the browser chrome adopts the Studio background */}
-        <meta
-          key="theme-color-light"
-          name="theme-color"
-          content={config.theme.color.light.default.base.bg}
-          media="(prefers-color-scheme: light)"
-        />
-        <meta
-          key="theme-color-dark"
-          name="theme-color"
-          content={config.theme.color.dark.default.base.bg}
-          media="(prefers-color-scheme: dark)"
-        />
-      </Head>
-    </>
-  )
+  return <StudioPageLayout config={config} />
 }

ff7b05119ef517370f5afaa19f225c008f6962c4
Cody Olsen <stipsan@gmail.com>
2022-08-16 15:41:04 +0200

    fix: update advanced example

--- examples/advanced/sanity.cli.ts
+++ examples/advanced/sanity.cli.ts
@@ -5,6 +5,8 @@ export default createCliConfig({
   api: { projectId: 'c8jibo38', dataset: 'themer-movies' },
   vite: (config: UserConfig): UserConfig => ({
     ...config,
-    build: { ...config.build, target: 'esnext' },
+    // Target needs to be es2022 to enable top-level await
+    // https://esbuild.github.io/content-types/#javascript:~:text=123n-,Top%2Dlevel%20await,-es2022
+    build: { ...config.build, target: 'es2022' },
   }),
 })

372ad56560bb52eefd8749b3f1d420dba896ffe5
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-10-11 00:46:42 +0200

    chore: update basic example

--- examples/basic/sanity.config.ts
+++ examples/basic/sanity.config.ts
@@ -1,3 +1,4 @@
+import { theme } from 'https://themer.sanity.build/api/hues?preset=verdant'
 import { createConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { muxInput } from 'sanity-plugin-mux-input'
@@ -6,9 +7,6 @@ import { schemaTypes } from './schemas'
 
 const projectId = import.meta.env.SANITY_STUDIO_API_PROJECT_ID
 const dataset = import.meta.env.SANITY_STUDIO_API_DATASET
-const { theme } = await import(
-  'https://themer.sanity.build/api/hues?preset=verdant'
-)
 
 export default createConfig({
   projectId,

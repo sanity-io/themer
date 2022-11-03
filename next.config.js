75927c85f079ed0a693cc3c8fda66e622d47c77f
Cody Olsen <cody@sanity.io>
2022-11-04 00:38:08 +0100

    chore: simplify config
    Signed-off-by: Cody Olsen <cody@sanity.io>

--- next.config.js
+++ next.config.js
@@ -5,22 +5,10 @@
  **/
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    browsersListForSwc: true,
-    legacyBrowsers: false,
-    // @TODO figure out why it fails due to not finding a 'critters' module
-    // optimizeCss: true,
-  },
-  compiler: {
-    styledComponents: true,
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
   images: {
     domains: ['cdn.sanity.io', 'source.unsplash.com'],
     formats: ['image/avif', 'image/webp'],
   },
-  // Disabling swcMinify until figuring out why the export modal crashes
-  // swcMinify: true,
 }
 
 module.exports = nextConfig

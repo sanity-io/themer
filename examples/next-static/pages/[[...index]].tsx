53de23e4ba53192afc5888a3f7a9646970325e76
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-22 09:57:39 +0100

    chore: refactor

--- examples/next-static/pages/[[...index]].tsx
+++ examples/next-static/pages/[[...index]].tsx
@@ -1,7 +1,16 @@
-import { NextStudio } from 'next-sanity/studio'
+import Head from 'next/head'
+import {NextStudio} from 'next-sanity/studio'
+import {NextStudioHead} from 'next-sanity/studio/head'
 
 import config from '../sanity.config'
 
 export default function IndexPage() {
-  return <NextStudio config={config} />
+  return (
+    <>
+      <Head>
+        <NextStudioHead />
+      </Head>
+      <NextStudio config={config} />
+    </>
+  )
 }
---
+++ examples/next-static/pages/_app.tsx
@@ -0,0 +1,3 @@
+import '../styles.css'
+
+export {default} from 'next/app'
